09f967a852d0088b78c6fcee3f312ea3ad5d3db0
adria15gomez <adria15gomez@gmail.com>
2023-01-12 18:40:59 +0100

    navbar estilos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../src/components/atoms/navbar/navbar.css";
 import Navbar from "../src/components/atoms/navbar/Navbar";
-
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
@@ -10,9 +9,6 @@ function App() {
       <Router>
         <Navbar />
       </Router>
-      <div className="main-content">
-        <h1> PRUEBA</h1>
-      </div>
     </div>
   );
 }
--- src/components/atoms/navbar/Navbar.jsx
+++ src/components/atoms/navbar/Navbar.jsx
@@ -2,35 +2,83 @@ import "../navbar/navbar.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../../img/logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function Navbar() {
   return (
     <div className="container">
       <div>
-        <img src={Logo} className="logo" />
+        <img src={Logo} className="Logo" />
       </div>
       <div>
         <nav>
-          <ul className="nav">
-            <NavLink to="/home" claseName>
-              <li>Home</li>
-            </NavLink>
-            <NavLink to="/prevention">
-              <li>Prevention</li>
-            </NavLink>
-            <NavLink to="/Quarentine">
-              <li>Quarentine</li>
-            </NavLink>
-            <NavLink to="/Pages">
-              <li>Pages</li>
-            </NavLink>
-            <NavLink to="/abaut">
-              <li>Abaut</li>
-            </NavLink>
-            <NavLink to="/help">
-              <li>Help</li>
-            </NavLink>
-          </ul>
+          <NavLink to="/home" class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+            >
+              Home
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="#">
+                  1 link
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  2 link
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  3 link
+                </a>
+              </li>
+            </ul>
+          </NavLink>
+          <NavLink to="/preventión">
+            <a>Prevention</a>
+          </NavLink>
+          <NavLink to="/Quarentine">
+            <a>Quarentine</a>
+          </NavLink>
+          <NavLink to="/Pages" class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+            >
+              Pages
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="#">
+                  1 link
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  2 link
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  3 link
+                </a>
+              </li>
+            </ul>
+          </NavLink>
+          <NavLink to="/abaut">
+            <a>Abaut</a>
+          </NavLink>
+          <NavLink to="/help">
+            <a>Help</a>
+          </NavLink>
         </nav>
       </div>
     </div>
