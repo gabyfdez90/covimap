f389d08a2985a2760b2713c9a74b55b18b07787d
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-01-12 14:30:21 +0100

    buttonDownload has been finilised

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react'
+import ButtonDownload from './components/atoms/buttonDownload/ButtonDownload'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <ButtonDownload background= "#0F0AA6" color = "white" />
+      <ButtonDownload background= "#3639AE" color = "white" />
     </div>
   );
 }
 
-export default App;
+export default App
