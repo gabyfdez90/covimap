8c317edc749af697badf171a2a0b0a7277809a15
TaniaPandal <psiqonautas@gmail.com>
2023-01-12 14:28:41 +0100

    share buttons styled missing click

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import ButtonShare from "./components/atoms/buttonShare/ButtonShare";
-import Cart from "./components/shareIcons/Untitled.png";
-import Headset from "./components/shareIcons/headset-solid 1.png";
-import Share from "./components/shareIcons/share-solid 1.png";
+
+
 
 
 
@@ -10,9 +8,6 @@ import Share from "./components/shareIcons/share-solid 1.png";
 function App() {
   return (
     <div className="App">
-      <ButtonShare color ="#FE6550" icon={Share}/>
-      <ButtonShare color ="#32D9BA" icon={Headset}/>
-      <ButtonShare color ="#94BF54" icon={Cart}/>
       
       </div>
     
