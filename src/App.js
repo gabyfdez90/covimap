c036a46b94c4b272badb0c7b29ec3e546d45c7f0
Ana <117080170+anecholls@users.noreply.github.com>
2023-01-12 14:25:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CardsTemplate from "./components/templates/CardsTemplate";
 
 function App() {
   return (
     <div className="App">
-      <CardsTemplate/>
     
     </div>
   );
