7fbd59070e76624d5e05d7ed7e3d465fc14b0c04
irene <irenefl897@gmail.com>
2023-01-13 12:51:56 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import '././components/atoms/footer/footer.css';
+import './components/molecules/landingContent6/landingContent6.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LandingContent6 from "./components/molecules/landingContent6/LandingContent6";
+import Footer from "./components/atoms/footer/Footer";
 
 
 function App() {
   return (
     <div className="App">
-      
+    <LandingContent6/>
+    
+    <Footer/>   
+
     </div>
   );
 }
