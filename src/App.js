1c6df01e4d15d6690ea8eb7b7235ee54c024df55
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-01-16 19:24:15 +0100

    Module landingContent5 finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      
     </div>
   );
 }
