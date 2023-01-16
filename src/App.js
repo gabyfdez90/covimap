2a78a4e913d269627de1e438761866ac1ddd8831
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-01-16 19:51:43 +0100

    Module LandingContent5 changed font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LandingContent5 from './components/molecules/landingContent5'
 
 
 function App() {
   return (
     <div className="App">
-      
+    
     </div>
   );
 }
