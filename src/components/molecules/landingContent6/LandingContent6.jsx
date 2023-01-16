633d2a1ebfdab7df1974930be3a014bd5b0d70d4
irene <irenefl897@gmail.com>
2023-01-16 12:11:29 +0100

    changes

--- src/components/molecules/landingContent6/LandingContent6.jsx
+++ src/components/molecules/landingContent6/LandingContent6.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import blog1 from '../../../img/blog1.jpg';
+import blog2 from '../../../img/blog2.jpg';
+import blog3 from '../../../img/blog3.jpg';
 
 function LandingContent6() {
     return (
-        <div>
-            <div className='containerf'>
-                <div class="containerLanding ">
-                <div class="row g-0 ">vhghjkhgjm</div>
-                <div class="row g-0 ">kfjkvkdjasdfas</div>
-                <div class="row g-0 ">kfjkvkdjasdfas</div>
-                </div>
-                </div>
+        <div className='molecula'>
             
+            <div className="containerLanding">
+                <div className="row align-items-center" >
+                    <img className='covid'src={blog1} style={{ width: '10%' }} /> <h3 id='text'>Can COVID-19 be caught from a person who has no symptoms?</h3>
+                </div>
+                <div className="row align-items-center">
+                <img className='mascarilla'src={blog2} style={{ width: '10%' }} /><h3 id='text'>What you need to know About Novel Corona Virus Covid 19</h3>
+                </div>
+                <div className="row align-items-center" >
+                <img className='resultado'src={blog3} style={{ width: '10%' }} /><h3 id='text'>What can I do to protect myself and prevent the spread of disease?</h3>
+                </div>
+            </div>
         </div>
     )
   }
