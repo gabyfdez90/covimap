c45e1b49884f3e6cdf37ec82b777b6fd04493425
adria15gomez <adria15gomez@gmail.com>
2023-01-17 14:08:17 +0100

    varios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LandingContent5 from "./components/molecules/landingContent5";
 
 function App() {
   return <div className="App"></div>;
