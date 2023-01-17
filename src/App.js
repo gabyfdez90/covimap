c92c4b9274bf357d2bac9cc6b35915d63cdb926d
adria15gomez <adria15gomez@gmail.com>
2023-01-17 08:55:04 +0100

    added Banner imagen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "../src/components/atoms/navbar/navbar.css";
 import Navbar from "../src/components/atoms/navbar/Navbar";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LandingContent1 from "./components/molecules/landingContent1/LandingContent1";
+import Banner from "./components/atoms/banner/Banner";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Router>
         <Navbar />
         <LandingContent1 />
+        <br></br>
+        <Banner />
       </Router>
     </div>
   );
