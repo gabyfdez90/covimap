3e2cd686f751b26c2aec18be1f2c929953cd6508
Gabriela M. Fernández <galalou2010@gmail.com>
2023-01-20 22:00:15 +0100

    merge fix 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import SideNavbar from "./components/atoms/sideNavbar/SideNavbar";
 
 function App() {
   return (
     <div className="App">
-      <Router></Router>
+      <Router>
+        <SideNavbar />
+      </Router>
     </div>
   );
 }
