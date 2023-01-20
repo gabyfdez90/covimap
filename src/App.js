c874f90aed3ef1a9c3d53cfb1a24c64a76d6d29a
Gabriela M. Fernández <galalou2010@gmail.com>
2023-01-20 21:50:45 +0100

    embeded map with iframe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Tracker1 from "./pages/Tracker/tracker1/Tracker1";
 
 function App() {
   return (
-    <BrowserRouter> 
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Tracker1 />
+      </div>
     </BrowserRouter>
   );
 }
