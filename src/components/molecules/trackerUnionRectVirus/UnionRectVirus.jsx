17bead2322d57e37f6865a0935cf36c664ad3226
irene <irenefl897@gmail.com>
2023-01-21 22:22:25 +0100

    union conter

---
+++ src/components/molecules/trackerUnionRectVirus/UnionRectVirus.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import CasesConter from '../../atoms/tracker/rectVirus/RectVirus';
+import './unionRectVirus.css'
+import { END_POINT } from "../../../services/endPoints";
+import { useFetch } from "../../../hooks/useFetch";
+
+function UnionRectVirus() {
+    const {data} = useFetch(`${END_POINT}/all`);
+
+    return (
+    <div className='UnionRectVirusContainer'>
+        <h2>COVID-19 Tracker</h2>
+        <CasesConter theme="theme" label="Total Cases" numberToday={`+${data.todayCases}`} numberTotal={data.cases} />
+        <CasesConter theme="theme" label="Active Cases" numberTotal={data.active} />
+        <CasesConter theme="theme" label="Recovered Cases" numberToday={`+${data.todayRecovered}`} numberTotal={data.recovered} />
+        <CasesConter theme="theme" label="Death Cases" numberToday={`+${data.todayDeaths}`} numberTotal={data.deaths} />
+    </div>
+  )
+}
+
+export default UnionRectVirus;
