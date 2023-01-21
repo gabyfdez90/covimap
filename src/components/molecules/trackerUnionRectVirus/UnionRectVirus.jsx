1d695f0f0e713d6b5f5b28dc82097a000a28c76c
irene <irenefl897@gmail.com>
2023-01-21 22:51:00 +0100

    changes

--- src/components/molecules/trackerUnionRectVirus/UnionRectVirus.jsx
+++ src/components/molecules/trackerUnionRectVirus/UnionRectVirus.jsx
@@ -11,9 +11,11 @@ function UnionRectVirus() {
     <div className='UnionRectVirusContainer'>
         <h2>COVID-19 Tracker</h2>
         <CasesConter theme="theme" label="Total Cases" numberToday={`+${data.todayCases}`} numberTotal={data.cases} />
-        <CasesConter theme="theme" label="Active Cases" numberTotal={data.active} />
-        <CasesConter theme="theme" label="Recovered Cases" numberToday={`+${data.todayRecovered}`} numberTotal={data.recovered} />
-        <CasesConter theme="theme" label="Death Cases" numberToday={`+${data.todayDeaths}`} numberTotal={data.deaths} />
+        <CasesConter theme="theme" label="Total Death" numberTotal={data.active} />
+        <CasesConter theme="theme" label="Total Recovered" numberToday={`+${data.todayRecovered}`} numberTotal={data.recovered} />
+        <CasesConter theme="theme" label="Total Active" numberToday={`+${data.todayDeaths}`} numberTotal={data.deaths} />
+        <CasesConter theme="theme" label="New Cases" numberToday={`+${data.todayDeaths}`} numberTotal={data.deaths} />
+        <CasesConter theme="theme" label="New Death" numberToday={`+${data.todayDeaths}`} numberTotal={data.deaths} />
     </div>
   )
 }
