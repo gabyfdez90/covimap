e3d989041e5db7a49b280ec3d299bd397006f8b9
irene <irenefl897@gmail.com>
2023-01-21 20:27:27 +0100

    new container

--- src/components/atoms/tracker/rectVirus/RectVirus.jsx
+++ src/components/atoms/tracker/rectVirus/RectVirus.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import React, { useState, useEffect } from 'react';
-import{MdOutlineCoronavirus} from'react-icons/md';
+import '../rectVirus/rectVirus.css'
 
 
-function CasesConter() {
-    const [data, setData] = useState([]);
-        useEffect(() => {
-        
-        fetch((`${END_POINT}/countries/usa`))
-            .then(response => response.json())
-            .then(data => {
-                setData(data);
-                
-            });
-    }, []);
 
-    return (
-        <Card style={{ width: '20rem' }}>
-        <Card.Body>
-            <Card.Title></Card.Title>
-             <MdOutlineCoronavirus id='covid'color='lightblue'/>
-            {/* <Card.Subtitle className="mb-2 text-muted">{totalCases}</Card.Subtitle> */}
-            {/* <Card.Text>{totalCases}</Card.Text> */}
-               
-            </Card.Body>
-        </Card>
-     
-   );
+function CasesConter({theme, label,number}) {
+    return(
+        <div id='casesConterContainer' className={theme}>
+            <div className='casesConterText'>{label}</div>
+            <div className='casesConterNumber'>{number}</div>
+
+
+        </div>
+    )
+   
 }
         
     
