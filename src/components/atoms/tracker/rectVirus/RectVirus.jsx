ba85395d2a638c62694a014d574f7b3378a28d4e
irene <irenefl897@gmail.com>
2023-01-21 21:27:27 +0100

    new conter

--- src/components/atoms/tracker/rectVirus/RectVirus.jsx
+++ src/components/atoms/tracker/rectVirus/RectVirus.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../rectVirus/rectVirus.css'
-
+import covidBlue from '../../../../img/icons/covid-blue.svg';
 
 
 function CasesConter({theme, label,number}) {
     return(
         <div id='casesConterContainer' className={theme}>
-            <div className='casesConterText'>{label}</div>
-            <div className='casesConterNumber'>{number}</div>
-
+            <div className='casesConterText'>{label='Total cases'}
+            <span><img className='covidBlue' src={covidBlue} /></span>
+            </div>
+            <div className='casesConterNumber'>{number=41302440}</div>
+            
+            
 
         </div>
     )
