b083c51dbeb5699e89a8a39d997d67e22a3ec98a
irene <irenefl897@gmail.com>
2023-01-21 22:54:21 +0100

    changes1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CasesConter from "./components/atoms/tracker/rectVirus/RectVirus";
-
+import UnionRectVirus from "./components/molecules/trackerUnionRectVirus/UnionRectVirus";
 
 function App() {
   return (
    
     <div className="App">
     <CasesConter/>
-
+    <UnionRectVirus/>
     </div>
     
   );
