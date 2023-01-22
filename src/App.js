6aea07f63b8c59565d496b59f0296f26d399d41d
TaniaPandal <psiqonautas@gmail.com>
2023-01-22 21:52:38 +0100

    linked LC6 css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import "././components/atoms/footer/footer.css";
-import "./components/molecules/landingContent6/landingContent6.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home"
 
 
 function App() {
   return (
-    
     <div className="App">
       <Home/>
     </div>
