da8e1ffa99f733585e3f6a85d88c246abe894ae3
Gabriela M. Fernández <galalou2010@gmail.com>
2023-01-22 17:40:22 +0100

    attempt to lines graphic

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tracker2 from "./pages/Tracker/tracker2/Tracker2";
 import Tracker6 from "./pages/Tracker/tracker6/Tracker6";
+import Tracker3 from "./pages/Tracker/tracker3/Tracker3";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Tracker2 />
+        <Tracker3 />
       </div>
     </Router>
   );
--- src/pages/Tracker/tracker3/Tracker3.jsx
+++ src/pages/Tracker/tracker3/Tracker3.jsx
@@ -1,13 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ApexCharts from 'react-apexcharts';
 import { useFetch } from "../../../hooks/useFetch";
+import ConsumoApi from '../../../services/ConsumoApi';
 import { END_POINT } from "../../../services/endPoints";
 
 function Tracker3() {
-    const {data} = useFetch(`${END_POINT}/historical/spain`)
-    console.log(data)
-    const countryList = data.timeline.cases
+    let dataSpain = useFetch(`${END_POINT}/historical/spain`)
+    console.log(dataSpain)
+    let evolutionSpain = Object.values(dataSpain.timeline.cases)
+    let dates = Object.keys(dataSpain.timeline.cases)
+    console.log(dates)
     
+    let dataUSA = useFetch(`${END_POINT}/historical/usa`)
+    console.log(dataUSA)
+    let evolutionUSA = Object.values(dataUSA.timeline.cases)
+    console.log(evolutionUSA)
+
+    let dataItaly = useFetch(`${END_POINT}/historical/italy`)
+    console.log(dataItaly)
+    let evolutionItaly = Object.values(dataItaly)
+
+    let dataGermany = useFetch(`${END_POINT}/historical/germany`)
+    console.log(dataGermany)
+    let evolutionGermany = Object.values(dataGermany) 
     
     const [options, setOptions] = useState({
         chart: {
@@ -16,19 +31,45 @@ function Tracker3() {
         },
         series: [{
           name: 'sales',
-          data: [{data}]
+          data: [evolutionUSA]
         }],
         xaxis: {
-          categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+          categories: dates
         }
 
       });
       
     return (
         <div id='testChart'>
-            <ApexCharts options={options} id='testChart' series={options.series} type='line'/>
+             <ApexCharts options={options} id='testChart' series={options.series} type='line'/> 
         </div>
   );
 }
 
 export default Tracker3
+
+    /* The upcoming code is a humble attempt to build a custom hook 
+     that upgrades the previous code. 
+    
+    const useEvolution = (country) => {
+      const [evolution, setEvolution] = useState([]);
+
+      useEffect(() => {
+        function setEvolution(country) {
+          let data = useFetch(`${END_POINT}/historical/${country}`).slice(0,10)
+          let evolution = Object.values(data.timeline.cases)
+          return evolution
+        };
+  });
+}
+
+const useDates = () => {
+  const [dates, setDates] = useState([])
+      
+  useEffect(() => {
+    function setDates() {
+      let data = useFetch(`${END_POINT}/historical/`).slice(0,10)
+      let dates = Object.keys(data.timeline.cases)
+      return dates
+    };
+  }); */
