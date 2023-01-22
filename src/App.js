377ef5385640c740acafaea86fd1fddcc84349a9
Gabriela M. Fernández <galalou2010@gmail.com>
2023-01-22 17:47:52 +0100

    ready to merge rectangles virus

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Tracker2 from "./pages/Tracker/tracker2/Tracker2";
-import Tracker6 from "./pages/Tracker/tracker6/Tracker6";
-import Tracker3 from "./pages/Tracker/tracker3/Tracker3";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Tracker3 />
-      </div>
+      <div className="App"></div>
     </Router>
   );
 }
