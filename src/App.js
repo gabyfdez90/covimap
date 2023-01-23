4fbff3367025e524cb63371c38cded2ed12bfc75
anecholls <anadelavegagonzalez@gmail.com>
2023-01-23 02:57:25 +0100

    rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
-
+import SideNavbar from "./components/atoms/sideNavbar/SideNavbar";
+import Tracker6 from "./pages/Tracker/tracker6/Tracker6";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from "./applications/Router";
 function App() {
   return (
     <div className="App">
+      <Router>
       <Home />
+      <Routes/>
+      </Router>
     </div>
   );
 }
